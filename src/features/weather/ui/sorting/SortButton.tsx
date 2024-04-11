bee522f002a1ac5d425b1bfb5cd179a9c56cc31d
Кирилл <kirillkozlove39@gmail.com>
2024-04-11 14:44:31 +0300

    features/ SortButton, SearchInput

---
+++ src/features/weather/ui/sorting/SortButton.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react';
+import sts from './SortButton.module.css';
+import cn from 'classnames';
+
+interface Props {
+  up?: boolean;
+}
+
+export const SortButton: FC<Props> = ({ up = false }) => {
+  const [letterAbove, letterBelow] = up ? ['A', 'Я'] : ['Я', 'А'];
+
+  return (
+    <button className={sts.sortButton}>
+      <div className={sts.arrowContainer}>
+        <div className={cn(sts.arrow, { [sts.arrowUp]: up })} />
+      </div>
+      <div>
+        {letterAbove} <br /> {letterBelow}
+      </div>
+    </button>
+  );
+};
