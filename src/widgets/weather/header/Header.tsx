2ce5bf3561dde43fdb996152878b17f2d39ca7a9
Кирилл <kirillkozlove39@gmail.com>
2024-04-11 14:45:15 +0300

    widgets/ Header

---
+++ src/widgets/weather/header/Header.tsx
@@ -0,0 +1,30 @@
+import React, { FC, useState } from 'react';
+import sts from './Header.module.css';
+import { Input } from '../../../shared/ui/input';
+import { Button } from '../../../shared/ui/button';
+import { SortButton } from '../../../features/weather/ui/sorting/SortButton';
+
+const Header: FC = () => {
+  const [search, setSearch] = useState<string>('');
+
+  return (
+    <header className={sts.header}>
+      <div className={sts.container}>
+        <section className={sts.sorting}>
+          <Button>Кнопка</Button>
+          <SortButton up />
+        </section>
+        <section>
+          <Input
+            value={search}
+            onChange={e => setSearch(e.target.value)}
+            onClear={() => setSearch('')}
+          />
+        </section>
+        <section>Много фильтров много фильтров много фильтров</section>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
